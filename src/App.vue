87119626293d9fb2b5cbc595749cc4a21bb338a6
GuaoHu <ghuu0007@student.monash.edu>
2025-08-08 19:57:55 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import JSON from './components/JSON.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <JSON/>
+    </div>
+  </header>
+
+</template>
+
+<style scoped></style>
